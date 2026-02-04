c304bf56b420c472a294e6a67b1f9abda3194097
simonkdev <simonkorten09@gmail.com>
2026-02-04 17:30:17 +0100

    asd

--- jellyfin-docker.nix
+++ jellyfin-docker.nix
@@ -3,26 +3,27 @@ pkgs.dockerTools.buildImage {
   name = "jellyfin-nix";
   tag = "latest";
 
-  contents = [
-    pkgs.bash
-    pkgs.coreutils
-    pkgs.jellyfin
-    pkgs.jellyfin-web
-    pkgs.jellyfin-ffmpeg
-  ];
-
-  config = {
-    ExposedPorts = {
-      "8096" = {};
-    };
-
-    Cmd = [
-      "${pkgs.jellyfin}/bin/jellyfin"
-      "--datadir"
-      "/config"
-      "--cachedir"
-      "/cache"
-      "--service"
-    ];
+  fromImage = pkgs.dockerTools.pullImage {
+    imageName = "jellyfin/jellyfin";
+    imageDigest = "sha256:333b647716631443a43c7fabac4b0c46b4e2f036bad19547e00958f10f721b85";
+    hash = "sha256-AnZWcBf+10Fo/9SEV4sO1bMtB0Bv4+tmg2nCs7F7uTM=";
+    finalImageName = "jellyfin/jellyfin";
+    finalImageTag = "latest";
   };
+
+  # contents = [
+  #   pkgs.bash
+  #   pkgs.stdenv.cc.cc.lib
+  # ];
+
+  # config = {
+  #   ExposedPorts = {
+  #     "8096" = {};
+  #   };
+
+  #   Cmd = [
+  #     "${pkgs.bash}/bin/bash"
+  #     "${pkgs.jellyfin}/bin/jellyfin--datadir /config --cachedir /cache --service"
+  #   ];
+  # };
 }
